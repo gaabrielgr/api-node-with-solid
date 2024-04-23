7ad9fa5e33ea91f51af229f8149d59b8162b30e9
gaabrielgr <gabrielgr.ti@gmail.com>
2024-04-23 20:40:03 -0300

    feat/remodelacao do banco de testes

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,58 +1,43 @@
-import 'dotenv/config';
-import { randomUUID } from 'node:crypto';
-import { Environment } from 'vitest';
-import { execSync } from 'node:child_process';
-import { PrismaClient } from '@prisma/client';
+import 'dotenv/config'
 
-const prisma = new PrismaClient();
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
 
-function generateDataBaseURL(schema: string) {
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error('Por favor defina a variável de ambiente DATABASE_URL');
+    throw new Error('Please provide a DATABASE_URL environment variable.')
   }
 
-  const url = new URL(process.env.DATABASE_URL);
+  const url = new URL(process.env.DATABASE_URL)
 
-  url.searchParams.set('schema', schema);
+  url.searchParams.set('schema', schema)
 
-  return url.toString();
+  return url.toString()
 }
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    // Acontece antes dos meus testes executarem
-    const schema = randomUUID();
-    const databaseURL = generateDataBaseURL(schema);
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
 
-    process.env.DATABASE_URL = databaseURL;
+    process.env.DATABASE_URL = databaseURL
 
-    execSync(`npx prisma migrate deploy`);
-
-    /* 
-    Devemos usar npx prisma migrate deploy 
-    o npx prisma migrate dev vai criar um banco de dados novo a cada vez que rodarmos os testes, o que não é o comportamento que queremos.
-    O deploy pula a etapa de verificação de se o banco de dados já existe, então ele não vai criar um novo banco de dados a cada vez que rodarmos os testes.
-    */
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        // depois dos meus testes executarem
-
         await prisma.$executeRawUnsafe(
-          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
-        );
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        )
 
-        await prisma.$disconnect();
+        await prisma.$disconnect()
       },
-    };
+    }
   },
-};
-
-/* 
-Ao rodar npm link dentro da pasta vitest-environment-prisma, o comando npm link vitest-environment-prisma é executado, criando um link simbólico para a pasta node_modules do projeto principal.
-Então quando voltamos para pasta principal e rodamos npm link vitest-environment-prisma, o comando npm link vitest-environment-prisma é executado, criando um link simbólico para a pasta node_modules do projeto vitest-environment-prisma.
-Dessa forma teremos um link simbólico entre as duas pastas, permitindo que o ambiente de testes do Prisma seja compartilhado entre os dois projetos.
-
-*/
+}
--- vite.config.mts
+++ vite.config.mts
@@ -4,6 +4,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    environmentMatchGlobs: [
+      [
+        'src/http/controllers/**',
+        './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+      ],
+    ],
   },
 });
